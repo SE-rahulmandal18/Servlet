cc168b8586d99aacfed4556834c62b3f2acb655b
Rahul Mandal <rahulbokaro610@gmail.com>
2023-05-30 08:53:53 +0530

    initial commit

---
+++ JDBCDemo/src/main/java/com/simpli/JDBCStoredProcedureDemo.java
@@ -0,0 +1,140 @@
+package com.simpli;
+
+import java.io.*;
+
+import java.sql.*;
+
+import java.util.*;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.annotation.WebServlet;
+
+import javax.servlet.http.*;
+
+@WebServlet("/callable-statement-demo")
+
+public class JDBCStoredProcedureDemo extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	DBUtil dbutil = null;
+
+	@Override
+
+	public void init() throws ServletException {
+
+		super.init();
+
+		InputStream in = getServletContext().getResourceAsStream("/WEB-INF/config.properties");
+
+		Properties props = new Properties();
+
+		try {
+
+			props.load(in);
+
+			dbutil = new DBUtil(props.getProperty("url"), props.getProperty("userid"), props.getProperty("password"));
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+
+			throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+
+		out.println("<html><body>");
+
+ 
+
+		// Get a DB connection
+
+		Connection connection = dbutil.getConnection();
+
+ 
+
+		// STEP 3 Create the Statement object.
+
+		try {
+
+			// STEP 3 Create the Prepared Statement object.
+
+			CallableStatement callableStmt = connection.prepareCall("{call add_product(?, ?)}");
+
+ 
+
+			callableStmt.setString(1, "Mac PC");
+
+			callableStmt.setFloat(2, 50000.25f);
+
+ 
+
+			callableStmt.execute();
+
+ 
+
+			out.println("Stored procedure has been executed.<Br>");
+
+			callableStmt.close();
+
+ 
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+
+ 
+
+	
+
+		// Insert new row demo
+
+		try {
+
+			// STEP 3 Create the Statement object.
+
+			PreparedStatement pStmt2 =
+
+					connection.prepareStatement("INSERT INTO eproduct(name,price) values(?, ?)");
+
+			pStmt2.setString(1, "Apple IPad");
+
+			pStmt2.setFloat(2, 20000.50f);
+
+			int count = pStmt2.executeUpdate();
+
+			out.println("Sucessfully added " + count + " row ");
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+	}
+
+	@Override
+
+	public void destroy() {
+
+		super.destroy();
+
+		try {
+
+			dbutil.closeConnection();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+}
